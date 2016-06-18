657261ca9ce20ea02c02068a78417ed131b90de9
MindfulBell <tim.bell02@gmail.com>
2016-06-18 16:35:26 -0700

    fixing IP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var main = require('./routes/index.js');
 
-app.set('trust proxy', 'loopback')
+app.set('trust proxy', true)
 app.use('/', main)
 
 module.exports = app;
